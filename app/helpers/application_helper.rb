3eeb2439efe27bad8af04affa200df6da5e4ed4b
Jon Gutwillig <7anders@gmail.com>
2011-05-17 13:57:35 -0400

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("png16.png", :alt => "Sample App", :class => "round")
+  end  
+
   # Return a title on a per-page basis
   def title
       base_title = "Twit Rails App"
